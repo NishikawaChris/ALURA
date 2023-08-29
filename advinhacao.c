0657616b3ab820ac3e6cd2249776e9d1b0323674
NishikawaChris <142899736+NishikawaChris@users.noreply.github.com>
2023-08-29 22:02:46 +0000

    Aplicação de loops e melhoria de código.

--- advinhacao.c
+++ advinhacao.c
@@ -6,22 +6,32 @@ printf("Seja bem vindo ao jogo de advinhação!\n");
 printf("*************************************\n");
 
 int numerosecreto = 42;
+
 int chute;
 
-    printf("Qual o seu chute? ");
-    scanf("%d", &chute);
-    printf("Seu chute foi %d\n", chute);
+for(int tentativa = 1; tentativa <= 3; tentativa++){
+        printf("Tentativa %d de 3\n", tentativa);
+        printf("Qual o seu chute? ");
+        scanf("%d", &chute);
+        printf("Seu chute foi %d\n", chute);
 
-    if(chute == numerosecreto) {
-        printf("Parabéns! Você acertou!\n");
-    }
-    else{
-        if(chute > numerosecreto) {
-            printf("Seu chute foi maior que o número secreto!\n");
+    int acertou = (chute == numerosecreto);
+
+        if(acertou) {
+            printf("Parabéns! Você acertou!\n");
+            //Para parar o programa quando o usuário acerta.
+            
+            break;
         }
-        if(chute<numerosecreto) {
-            printf("Seu número é menor que o número secreto!\n");
-        }
-        
+        else{
+            int maior = (chute > numerosecreto);
+            if(maior) {
+                printf("Seu chute foi maior que o número secreto!\n");
+            } else{
+                printf("Seu número é menor que o número secreto!\n");
+            }
+        }  
     }
+
+    printf("FIM DE JOGO!!!\n");
 }
