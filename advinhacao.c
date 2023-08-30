44daf4bdf08325e3501a97fcccf3547f790f188d
NishikawaChris <142899736+NishikawaChris@users.noreply.github.com>
2023-08-30 02:21:04 +0000

    Sem restringir número de tentativas até erro

--- advinhacao.c
+++ advinhacao.c
@@ -2,7 +2,7 @@
 
 //Tudo que vem acompanhado de # chama-se diretiva.
 //Por convenção, escreve-se as constantes em caixa alta e separadas por underscore.
-#define NUMERO_DE_TENTATIVA 5
+//#define NUMERO_DE_TENTATIVA 5
 
 int main(){
 printf("*************************************\n");
@@ -12,9 +12,14 @@ printf("*************************************\n");
 int numerosecreto = 42;
 
 int chute;
+int ganhou = 0;
+int tentativas = 1;
 
-for(int tentativa = 1; tentativa <= NUMERO_DE_TENTATIVA; tentativa++){
-        printf("Tentativa %d de %d\n", tentativa, NUMERO_DE_TENTATIVA);
+//O "for" é um loop ideal para quando o usuário sabe o número exato de tentativas.
+//for(int tentativa = 1; tentativa <= NUMERO_DE_TENTATIVA; tentativa++){
+//Com a variável while
+while(ganhou == 0) {
+        printf("Tentativa %d\n", tentativas);
         printf("Qual o seu chute? ");
         scanf("%d", &chute);
         printf("Seu chute foi %d\n", chute);
@@ -22,7 +27,7 @@ for(int tentativa = 1; tentativa <= NUMERO_DE_TENTATIVA; tentativa++){
     //Este código serve para impedir que o usuário informe um número negativo.
     if(chute < 0) {
         printf("Você não pode informar um número negativo!\n");
-        tentativa--;
+       //tentativa--;
 
         //Este código faz o programa parar na execução do bloco e ir direto par ao "tentativa++"
         continue;
@@ -30,13 +35,14 @@ for(int tentativa = 1; tentativa <= NUMERO_DE_TENTATIVA; tentativa++){
 
     int acertou = (chute == numerosecreto);
     int maior = chute > numerosecreto;
-    int menor = chute < numerosecreto;
+   //int menor = chute < numerosecreto;
 
         if(acertou) {
             printf("Parabéns! Você acertou!\n");
             
             //Para parar o programa quando o usuário acerta.
-            break;
+           //break;
+           ganhou = 1;
         }
       
       //Se a condição de um else if for verdadeira, o programa não irá mais compilar os demais.
@@ -46,8 +52,12 @@ for(int tentativa = 1; tentativa <= NUMERO_DE_TENTATIVA; tentativa++){
             
         else {
             printf("Seu número é menor que o número secreto!\n");
-        }  
+        } 
+
+        tentativas = tentativas + 1;
     }
 
     printf("FIM DE JOGO!!!\n");
+
+    printf("Você acertou o número secreto em %d tentativas!\n", tentativas);
 }
